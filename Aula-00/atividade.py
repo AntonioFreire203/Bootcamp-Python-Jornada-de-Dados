66cb2aaa6658e8713f4e724f8bd50ece78fc0a55
AntonioFreire203 <freireantonio203@gmail.com>
2024-10-23 15:07:26 -0300

    update readme.md

--- Aula-00/atividade.py
+++ Aula-00/atividade.py
@@ -7,13 +7,17 @@ print(programadores)
 #programadores.append('Antonio')
 #print(programadores)
 
-#2-1-Adicionar elementos (insert('posição','elemento') )
+#2-1-Adicionar um elemento elemento (insert('posição','elemento') )
 #programadores.insert(1,'segunda')
 #print(programadores)
 #['Victor', 'Juliana', 'Samuel', 'Caio', 'Luana']
 #['Victor', 'segunda', 'Juliana', 'Samuel', 'Caio', 'Luana']
 
-#3-1-remove() para a remoção pelo valor informado no parâmetro 
-programadores.remove('Victor')
-print(programadores)
+#3-1-remove() para de um elemento remoção pelo valor informado no parâmetro 
+#programadores.remove('Victor')
+#print(programadores)
+#saida:
+
 #3-2 -pop() para remoção pelo índice do elemento na lista
+#programadores.pop(0)
+#print(programadores)
